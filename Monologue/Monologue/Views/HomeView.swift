6fdeed23d0d2a700b0e5d466c69019911e584408
First Snow <show2633@naver.com>
2024-10-14 19:16:03 +0900

    [Add]: Project Base
    1. MainView
     - ContentView -> MainView 이름 변경
     - 각 View를 띄워 줄 TabView 추가
    2. HomeView 추가
    3. PostView 추가
    4. MyPageView 추가

---
+++ Monologue/Monologue/Views/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  Monologue
+//
+//  Created by Hyunwoo Shin on 10/14/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color(.accent)
+                .ignoresSafeArea()
+            
+            Text("HomeView")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Monologue/Monologue/Views/MyPageView.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageView.swift
+//  Monologue
+//
+//  Created by Hyunwoo Shin on 10/14/24.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        ZStack {
+            Color(.accent)
+                .ignoresSafeArea()
+            
+            Text("MyPageView")
+        }
+    }
+}
+
+#Preview {
+    MyPageView()
+}
