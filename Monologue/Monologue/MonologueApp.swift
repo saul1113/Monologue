92fa7ca6e144d8e163fdc17819cdb1fbaed444e5
First Snow <show2633@naver.com>
2024-10-14 17:40:11 +0900

    [Add]: Init

---
+++ Monologue/Monologue/MonologueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonologueApp.swift
+//  Monologue
+//
+//  Created by Hyunwoo Shin on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MonologueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
