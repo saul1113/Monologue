f929999a8f4602045ab5690a9fb58b3a5df69dd6
Hyojeong <abadeer@naver.com>
2024-10-15 13:30:46 +0900

    [Design]: ProfileEditView 작성
    1. ProfileEditView 디자인 끝
    2. MyPageView 최상위에 NavigationStack 추가해서 NavigationLink로 뷰 연결

--- Monologue/Monologue/Views/ProfileEditView.swift
+++ Monologue/Monologue/Views/ProfileEditView.swift
@@ -8,11 +8,77 @@
 import SwiftUI
 
 struct ProfileEditView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var nickname: String = "" // 파베로 변경 예정
+    @State private var introduction: String = "" // 파베로 변경 예정
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.background)
+                .ignoresSafeArea()
+            
+            VStack {
+                Circle() // profileImageName
+                    .frame(width: 77, height: 77)
+                    .padding(.bottom, 17)
+                    .padding(.top, 20)
+                
+                Button {
+                    
+                } label: {
+                    Text("사진 수정")
+                        .bold()
+                }
+                .padding(.bottom, 53)
+                
+                HStack {
+                    Text("닉네임")
+                        .padding(.trailing, 30)
+                        .bold()
+                    
+                    TextField("닉네임 변경", text: $nickname)
+                }
+                
+                Divider()
+                    .padding(.bottom, 18)
+                
+                HStack {
+                    Text("자기소개")
+                        .padding(.trailing, 15)
+                        .bold()
+                    
+                    TextField("자기소개 추가", text: $introduction)
+                }
+                
+                Divider()
+                
+                Spacer()
+            }
+            .padding(.horizontal, 16)
+            .foregroundStyle(.accent)
+        }
+        .navigationBarBackButtonHidden(true) // 기본 백 버튼 숨기기
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("완료") {
+                    // 프로필 변경 저장 로직
+                    dismiss()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ProfileEditView()
+    NavigationStack {
+        ProfileEditView()
+    }
 }
