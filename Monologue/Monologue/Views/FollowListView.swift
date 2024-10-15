8411688f4584296cdafe447a0b9103bff92f69c4
Hyojeong <abadeer@naver.com>
2024-10-16 00:00:50 +0900

    [Feat]: 메모 및 칼럼 데이터 연결
    메모 및 칼럼 View를 수정 중이시라 뷰 연결하긴 애매해서 일단 pr 하겠습니다!

--- Monologue/Monologue/Views/FollowListView.swift
+++ Monologue/Monologue/Views/FollowListView.swift
@@ -23,38 +23,7 @@ struct FollowListView: View {
             Color(.background)
                 .ignoresSafeArea()
             
-            // 커스텀 Segment
-            GeometryReader { geometry in
-                HStack {
-                    ForEach(segments, id: \.self) { segment in
-                        Button {
-                            withAnimation(.easeInOut(duration: 0.3)) {
-                                selectedSegment = segment
-                            }
-                        } label: {
-                            Text(segment)
-                                .font(.system(size: 16))
-                                .foregroundStyle(.accent)
-                                .frame(maxWidth: .infinity) // 각 탭을 동일한 너비로 설정
-                        }
-                    }
-                }
-                .overlay(
-                    // 전체 너비 밑줄
-                    Rectangle()
-                        .fill(.accent.opacity(0.2))
-                        .frame(width: geometry.size.width, height: 1) // 전체 너비
-                        .offset(y: 13),
-                    alignment: .bottomLeading
-                )
-                .overlay(
-                    Rectangle()
-                        .fill(.accent)
-                        .frame(width: geometry.size.width / 2, height: 2)
-                        .offset(x: selectedSegment == "팔로워" ? 0 : geometry.size.width / 2, y: 13),
-                    alignment: .bottomLeading
-                )
-            }
+            CustomSegmentView(segment1: "팔로워", segment2: "팔로잉", selectedSegment: $selectedSegment)
             
             ScrollView {
                 VStack {
