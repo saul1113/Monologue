46f2bbcdacb822bbf4860683665d655a81ceb946
Min <jmsj0308@naver.com>
2024-10-15 10:40:39 +0900

    [Add]: MemoWritingView 파일 추가

---
+++ Monologue/Monologue/Views/MemoWritingView.swift
@@ -0,0 +1,18 @@
+//
+//  MemoWritingView.swift
+//  Monologue
+//
+//  Created by Min on 10/15/24.
+//
+
+import SwiftUI
+
+struct MemoWritingView: View {
+    var body: some View {
+        Text("MemoWritingView")
+    }
+}
+
+#Preview {
+    MemoWritingView()
+}
