b077a097aca82ca1fcd9bfce57146b72b07b9770
Hyojeong <abadeer@naver.com>
2024-10-15 14:32:17 +0900

    [Design]: SettingView 작성

--- Monologue/Monologue/Views/SettingView.swift
+++ Monologue/Monologue/Views/SettingView.swift
@@ -8,15 +8,70 @@
 import SwiftUI
 
 struct SettingView: View {
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
         ZStack {
             Color(.background)
                 .ignoresSafeArea()
-            Text("Hello, World!")
+            
+            VStack {
+                List {
+                    Section("사용자 설정") {
+                        NavigationLink("좋아요 목록") {
+                            
+                        }
+                        
+                        NavigationLink("차단 유저 목록") {
+                            
+                        }
+                        
+                        Button("로그아웃") {
+                            // 파베 로직
+                            print("로그아웃")
+                        }
+                        
+                        Button("계정 탈퇴") {
+                            // 파베 로직
+                            print("계정 탈퇴")
+                        }
+                        .foregroundStyle(.red)
+                    }
+                    .listRowBackground(Color(.background))
+                    
+                    Section("기타") {
+                        HStack {
+                            Text("앱 버전")
+                            
+                            Spacer()
+                            
+                            Text("1.0")
+                                .bold()
+                        }
+                    }
+                    .listRowBackground(Color(.background))
+                }
+                .listStyle(.plain)
+            }
+            .foregroundStyle(.accent)
+        }
+        .navigationTitle("설정")
+        .toolbarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true) // 기본 백 버튼 숨기기
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                }
+            }
         }
     }
 }
 
 #Preview {
-    SettingView()
+    NavigationStack {
+        SettingView()
+    }
 }
