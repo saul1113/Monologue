d6f8c7fcc137358138258520fe1f435f2f9633db
First Snow <show2633@naver.com>
2024-10-15 17:25:34 +0900

    [Add]: MemoStore 작업
    1. addMemo
     - 메모 전체 추가, 수정
    2. loadMemos
     - 메모 전체 로드
    3. loadMemosByUserNickname
     - 메모 유저 닉네임으로 로드
    4. loadMemosByCategories
     - 메모 카테고리들로 로드
    5. deleteMemo
     - 메모 아이디로 메모 삭제
    6. updateLikes
     - 좋아요 수정
    7. updateComment
     - 댓글 수정

--- Monologue/Monologue/Model/Memo.swift
+++ Monologue/Monologue/Model/Memo.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseFirestore
 
 struct Memo: Codable, Identifiable {
     var id: String = UUID().uuidString
@@ -17,4 +19,34 @@ struct Memo: Codable, Identifiable {
     var likes: [String] // 좋아요 개수
     var comments: [String] // 코멘트ID
     var date: Date // 날짜
+    
+    init(document: QueryDocumentSnapshot) {
+        let docData = document.data()
+        
+        self.id = document.documentID
+        self.content = docData["content"] as? String ?? ""
+        self.userNickname = docData["userNickname"] as? String ?? ""
+        self.font = docData["font"] as? String ?? ""
+        self.backgroundImageName = docData["backgroundImageName"] as? String ?? ""
+        self.categories = docData["categories"] as? [String] ?? []
+        self.likes = docData["likes"] as? [String] ?? []
+        self.comments = docData["comments"] as? [String] ?? []
+        
+        if let timestamp = docData["date"] as? Timestamp {
+            self.date = timestamp.dateValue()
+        } else {
+            self.date = Date()
+        }
+    }
+    
+    init(content: String, userNickname: String, font: String, backgroundImageName: String, categories: [String], likes: [String], comments: [String], date: Date) {
+        self.content = content
+        self.userNickname = userNickname
+        self.font = font
+        self.backgroundImageName = backgroundImageName
+        self.categories = categories
+        self.likes = likes
+        self.comments = comments
+        self.date = date
+    }
 }
