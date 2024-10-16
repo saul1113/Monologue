9d43fd5ab3dc5a7c653caea56fc019bbbc7786de
Hyojeong <abadeer@naver.com>
2024-10-17 01:50:09 +0900

    [Add]: UserProfileView 파일 추가
    다른 유저 프로필 보는 뷰

--- Monologue/Monologue/Views/MyPageViews/UserProfileView.swift
+++ Monologue/Monologue/Views/MyPageViews/UserProfileView.swift
@@ -1,22 +1,29 @@
 //
-//  MyPageView.swift
+//  UserProfileView.swift
 //  Monologue
 //
-//  Created by Hyunwoo Shin on 10/14/24.
+//  Created by Hyojeong on 10/17/24.
 //
 
 import SwiftUI
 
-struct MyPageView: View {
-    @EnvironmentObject private var userInfoStore: UserInfoStore
-    @EnvironmentObject private var authManager:AuthManager
+// 다른 유저들 프로필 뷰
+struct UserProfileView: View {
+    public let userInfo: UserInfo
+    
     @EnvironmentObject private var memoStore: MemoStore
     @EnvironmentObject private var columnStore: ColumnStore
     
+    @Environment(\.dismiss) private var dismiss
     @State var selectedSegment: String = "메모"
     @State private var userMemos: [Memo] = [] // 사용자가 작성한 메모들
     @State private var userColumns: [Column] = [] // 사용자가 작성한 칼럼들
     private var sharedString: String = "MONOLOG" // 변경 예정
+    
+    // 기본 이니셜라이저
+    init(userInfo: UserInfo) {
+        self.userInfo = userInfo
+    }
     
     var body: some View {
         NavigationStack {
@@ -26,24 +33,19 @@ struct MyPageView: View {
                 
                 VStack {
                     HStack(spacing: 20) {
-                        Text("MONOLOG")
-                            .foregroundStyle(.accent)
-                            .font(.title3)
-                            .bold()
-                        
-                        Spacer()
-                        
-                        NavigationLink {
-                            NotificationView()
+                        Button {
+                            dismiss()
                         } label: {
-                            Image(systemName: "bell")
+                            Image(systemName: "chevron.backward")
                                 .font(.title3)
                         }
                         
-                        NavigationLink {
-                            SettingView()
+                        Spacer()
+                        
+                        Button {
+                            
                         } label: {
-                            Image(systemName: "line.3.horizontal")
+                            Image(systemName: "ellipsis")
                                 .font(.title3)
                         }
                     }
@@ -51,16 +53,16 @@ struct MyPageView: View {
                     // 프사, 닉, 상메
                     HStack {
                         // 프로필 사진
-                        ProfileImageView(profileImageName: userInfoStore.userInfo?.profileImageName ?? "",
+                        ProfileImageView(profileImageName: userInfo.profileImageName,
                                          size: 77)
-                            .padding(.trailing, 24)
+                        .padding(.trailing, 24)
                         
                         VStack(alignment: .leading, spacing: 12) {
-                            Text(userInfoStore.userInfo?.nickname ?? "닉네임 없음")
+                            Text(userInfo.nickname)
                                 .font(.system(size: 18))
                                 .bold()
                             
-                            Text(userInfoStore.userInfo?.introduction ?? "자기소개가 없습니다.")
+                            Text(userInfo.introduction.isEmpty ? "자기소개가 없습니다." : userInfo.introduction)
                                 .font(.system(size: 16))
                         }
                     }
@@ -72,7 +74,7 @@ struct MyPageView: View {
                     HStack(spacing: 20) {
                         HStack {
                             Text("메모")
-                            Text("\(userInfoStore.getMemoCount(userNickname: userInfoStore.userInfo?.nickname ?? ""))") // Memo 개수
+                            Text("")
                                 .bold()
                         }
                         .padding(.horizontal, 2)
@@ -81,7 +83,7 @@ struct MyPageView: View {
                         
                         HStack {
                             Text("칼럼")
-                            Text("\(userInfoStore.getColumnCount(userNickname: userInfoStore.userInfo?.nickname ?? ""))") // Column 개수
+                            Text("")
                                 .bold()
                         }
                         .padding(.horizontal, 2)
@@ -93,7 +95,7 @@ struct MyPageView: View {
                         } label: {
                             HStack {
                                 Text("팔로워")
-                                Text("\(userInfoStore.userInfo?.followers.count ?? 0)")
+                                Text("\(userInfo.followers.count)")
                                     .bold()
                             }
                             .padding(.horizontal, 2)
@@ -106,11 +108,11 @@ struct MyPageView: View {
                         } label: {
                             HStack {
                                 Text("팔로잉")
-                                Text("\(userInfoStore.userInfo?.followings.count ?? 0)")
+                                Text("\(userInfo.followings.count)")
                                     .bold()
                             }
                             .padding(.horizontal, 2)
-                        } 
+                        }
                     }
                     .font(.system(size: 14))
                     .frame(height: 22)
@@ -118,10 +120,10 @@ struct MyPageView: View {
                     
                     // 프로필 편집, 공유 버튼
                     HStack {
-                        NavigationLink {
-                            ProfileEditView()
+                        Button {
+                            // 팔로 or 언팔 로직
                         } label: {
-                            Text("프로필 편집")
+                            Text("팔로잉") // 팔로우 상태에 따라 텍스트 변경하도록 바꿔야 됨...
                                 .font(.system(size: 15))
                                 .frame(maxWidth: .infinity, minHeight: 30)
                                 .background(RoundedRectangle(cornerRadius: 10)
@@ -129,8 +131,10 @@ struct MyPageView: View {
                                 )
                         }
                         
-                        ShareLink(item: sharedString) {
-                            Text("프로필 공유")
+                        Button {
+                            
+                        } label: {
+                            Text("알림 설정")
                                 .font(.system(size: 15))
                                 .frame(maxWidth: .infinity, minHeight: 30)
                                 .background(RoundedRectangle(cornerRadius: 10)
@@ -147,7 +151,7 @@ struct MyPageView: View {
                         MemoView(filteredMemos: userMemos)
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
                             .padding(.horizontal, -16)
-
+                        
                     } else if selectedSegment == "칼럼" {
                         // 칼럼 뷰
                         ColumnView(filteredColumns: userColumns)
@@ -160,18 +164,13 @@ struct MyPageView: View {
             }
             .onAppear {
                 Task {
-                    // 유저의 정보 로드
-                    await userInfoStore.loadUserInfo(email: authManager.email)
-                    
-                    // 유저의 메모 로드
-                    memoStore.loadMemosByUserNickname(userNickname: userInfoStore.userInfo?.nickname ?? "") { memos, error in
+                    memoStore.loadMemosByUserNickname(userNickname: userInfo.nickname) { memos, error in
                         if let memos = memos {
                             userMemos = memos
                         }
                     }
                     
-                    // 유저의 칼럼 로드
-                    columnStore.loadColumnsByUserNickname(userNickname: userInfoStore.userInfo?.nickname ?? "") { columns, error in
+                    columnStore.loadColumnsByUserNickname(userNickname: userInfo.nickname) { columns, error in
                         if let columns = columns {
                             userColumns = columns
                         }
@@ -182,12 +181,14 @@ struct MyPageView: View {
     }
 }
 
-//#Preview {
-//    NavigationStack {
-//        MyPageView()
-//            .environmentObject(AuthManager())
-//            .environmentObject(UserInfoStore())
-//            .environmentObject(MemoStore())
-//            .environmentObject(ColumnStore())
-//    }
-//}
+#Preview {
+    UserProfileView(userInfo: UserInfo(nickname: "피곤해",
+                                       registrationDate: Date(),
+                                       preferredCategories: [],
+                                       profileImageName: "profileImage2",
+                                       introduction: "자고 싶어요.",
+                                       followers: [],
+                                       followings: [],
+                                       blocked: [],
+                                       likes: []))
+}
