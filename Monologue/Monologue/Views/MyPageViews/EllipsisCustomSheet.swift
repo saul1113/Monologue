579f70992090d8837fdf0afb7014af4ff71e8a99
Hyojeong <abadeer@naver.com>
2024-10-19 03:28:40 +0900

    [Add]: EllipsisCustomSheet 추가
    EllipsisCustomSheet에 대한 설명은 주석으로 적어놨습니다!
    (해당 시트를 메모 및 칼럼 디테일 뷰에서도 사용해서 만들어뒀습니다.)
    
    나중에 머지하실 때 EllipsisCustomSheet 파일 ViewBuilders 쪽을 옮겨주시면 감사하겠습니다 ,, ㅎㅎ

---
+++ Monologue/Monologue/Views/MyPageViews/EllipsisCustomSheet.swift
@@ -0,0 +1,300 @@
+//
+//  EllipsisCustomSheet.swift
+//  Monologue
+//
+//  Created by Hyojeong on 10/18/24.
+//
+/*
+ 커스텀 시트 설명:
+ Image(systemName: "ellipsis") 버튼을 눌렀을 때 나오는 시트입니다.
+ 
+ 시트 내 사용하고자 하는 버튼의 이름은 SheetButtonOption의 title에 쓰고,
+ 버튼의 액션은 SheetButtonOption의 action에 쓰면 됩니다.
+ 
+ sharedString은 공유하기 버튼(ShareLink)에 필요한 상수이기 때문에
+ 공유하기 버튼을 쓰지 않는다면 nil로 써주시기 바랍니다.
+ */
+
+import SwiftUI
+
+struct SheetButtonOption: Identifiable {
+    let id: UUID = UUID()
+    let type: ButtonOptionType // 버튼 이름
+    let action: () -> Void // 버튼 액션
+}
+
+enum ReportOrDeleteTitle: String {
+    case memo = "메모"
+    case column = "칼럼"
+    case comment = "댓글"
+    case user = "회원"
+}
+
+enum ButtonOptionType: String {
+    case share = "공유하기"
+    case report = "신고하기"
+    case block = "차단하기"
+    case delete = "삭제하기"
+    case cancel = "취소"
+}
+
+struct EllipsisCustomSheet: View {
+    let buttonOptions: [SheetButtonOption]
+    let sharedString: String? /// 공유하고자 하는 String
+    let sharedImage: Image = Image(.appLogo)
+    let reportOrDeleteTitle: ReportOrDeleteTitle /// 신고 or 삭제 대상: user, memo, column, comment 중 하나 적어주세요(enum 값임)
+    @Binding var isShowingReportSheet: Bool
+    @Binding var isShowingBlockAlert: Bool
+    @Binding var isShowingEllipsisSheet: Bool
+    @Binding var isShowingDeleteAlert: Bool
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(buttonOptions) { option in
+                switch option.type {
+                case .share:
+                    ShareLink(
+                        item: sharedImage,
+                        preview: SharePreview(sharedString!, image: sharedImage)
+                    ) {
+                        Text(option.type.rawValue)
+                            .foregroundStyle(.black)
+                            .frame(maxWidth: .infinity)
+                    }
+                    
+                case .report:
+                    Button {
+                        isShowingReportSheet.toggle()
+                    } label: {
+                        Text(option.type.rawValue)
+                            .frame(maxWidth: .infinity)
+                            .foregroundStyle(.red)
+                    }
+                    .sheet(isPresented: $isShowingReportSheet) {
+                        ReportSheet(isShowingEllipsisSheet: $isShowingEllipsisSheet,
+                                    isShowingReportSheet: $isShowingReportSheet,
+                                    reportTitle: reportOrDeleteTitle)
+                        .presentationDetents([.height(550)])
+                    }
+                    
+                case .block:
+                    Button {
+                        isShowingBlockAlert.toggle()
+                    } label: {
+                        Text(option.type.rawValue)
+                            .frame(maxWidth: .infinity)
+                            .foregroundStyle(.red)
+                    }
+                    .alert(isPresented: $isShowingBlockAlert) {
+                        Alert(
+                            title: Text("차단하기"),
+                            message: Text("차단된 사람은 회원님의 프로필 또는 콘텐츠를 볼 수 없게 됩니다."),
+                            primaryButton: .destructive(Text("차단")) {
+                                option.action()
+                                isShowingEllipsisSheet = false
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    }
+                    
+                case .delete:
+                    Button {
+                        isShowingDeleteAlert.toggle()
+                    } label: {
+                        Text(option.type.rawValue)
+                            .frame(maxWidth: .infinity)
+                            .foregroundStyle(.red)
+                    }
+                    .alert(isPresented: $isShowingDeleteAlert) {
+                        Alert(
+                            title: Text("삭제하기"),
+                            message: Text(getdeleteTitleText(for: reportOrDeleteTitle)),
+                            primaryButton: .destructive(Text("삭제")) {
+                                option.action()
+                                isShowingEllipsisSheet = false
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    }
+
+                case .cancel:
+                    Button {
+                        option.action()
+                    } label: {
+                        Text(option.type.rawValue)
+                            .foregroundStyle(.black)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                
+                if option.type != .cancel {
+                    Divider()
+                        .padding(.horizontal, 16)
+                }
+            }
+        }
+        .padding(.bottom, 10)
+        .frame(maxHeight: .infinity, alignment: .bottom)
+    }
+    
+    // deleteTitle에 따라 다른 텍스트를 반환하는 함수
+    private func getdeleteTitleText(for title: ReportOrDeleteTitle) -> String {
+        switch title {
+        case .memo:
+            return "이 \(title.rawValue)는 영구적으로 삭제되며 복원할 수 없습니다."
+            
+        case .column, .comment, .user:
+            return "이 \(title.rawValue)은 영구적으로 삭제되며 복원할 수 없습니다."
+        }
+    }
+}
+
+// MARK: - 신고하는 이유 시트
+struct ReportSheet: View {
+    @Binding var isShowingEllipsisSheet: Bool
+    @Binding var isShowingReportSheet: Bool
+    
+    let reportTitle: ReportOrDeleteTitle
+    
+    let reasons = [
+        "상업적 홍보 및 광고",
+        "비속어 및 욕설",
+        "혐오 및 음란 내용 신고",
+        "도배 신고",
+        "정치 및 분란 유도 신고",
+        "기타 문제 신고"
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Text(ReportCompleteSheet.getReportTitleText(for: reportTitle))
+                    .font(.title3)
+                    .bold()
+                
+                Text("회원님의 신고는 익명으로 처리됩니다.")
+                    .foregroundStyle(.secondary)
+                    .font(.subheadline)
+                    .padding(.top, -10)
+                    .padding(.bottom, 10)
+                
+                Divider()
+                
+                // 신고 버튼 목록
+                ForEach(reasons, id: \.self) { reason in
+                    NavigationLink {
+                        ReportCompleteSheet(reason: reason,
+                                            reportTitle: reportTitle,
+                                            isShowingEllipsisSheet: $isShowingEllipsisSheet,
+                                            isShowingReportSheet: $isShowingReportSheet)
+                    } label: {
+                        HStack {
+                            Text(reason)
+                            Spacer()
+                            Image(systemName: "chevron.forward")
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                    Divider()
+                }
+            }
+            .padding(.bottom, 20)
+            .padding(.horizontal, 16)
+            .foregroundStyle(.black)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+    }
+}
+
+// MARK: - 신고 제출 시트
+struct ReportCompleteSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    let reason: String
+    let reportTitle: ReportOrDeleteTitle
+    @State private var customReason: String = ""
+    @Binding var isShowingEllipsisSheet: Bool
+    @Binding var isShowingReportSheet: Bool
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("신고를 제출합니다.")
+                .font(.title3)
+                .bold()
+            
+            Text("커뮤니티 가이드라인을 위반하는 콘텐츠만 삭제됩니다. 아래에서 신고 상세 정보를 검토하거나 수정할 수 있습니다.")
+                .foregroundStyle(.secondary)
+                .font(.subheadline)
+                .padding(.bottom, 10)
+            
+            Divider()
+            
+            Group {
+                Text("신고 상세 정보")
+                    .font(.headline)
+                
+                Text(ReportCompleteSheet.getReportTitleText(for: reportTitle))
+                
+                if reason == "기타 문제 신고" {
+                    TextField("자세한 신고 이유를 입력하세요.", text: $customReason)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.top, -10)
+                } else {
+                    Text(reason)
+                        .foregroundStyle(.secondary)
+                        .font(.subheadline)
+                        .padding(.top, -12)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+                isShowingEllipsisSheet = false
+                isShowingReportSheet = false
+            } label: {
+                Text("제출")
+                    .frame(maxWidth: .infinity, minHeight: 35)
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding(.bottom, 10)
+        .padding(.horizontal, 16)
+        .frame(maxHeight: .infinity, alignment: .bottom)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                }
+            }
+        }
+    }
+    
+    // reportTitle에 따라 다른 텍스트를 반환하는 함수
+    public static func getReportTitleText(for title: ReportOrDeleteTitle) -> String {
+        switch title {
+        case .memo:
+            return "이 \(title.rawValue)를 신고하는 이유"
+        case .column, .comment, .user:
+            return "이 \(title.rawValue)을 신고하는 이유"
+        }
+    }
+}
+
+#Preview("EllipsisCustomSheet") {
+    EllipsisCustomSheet(buttonOptions: [SheetButtonOption(type: .share, action: { print("공유하기 clicked") }),
+                                        SheetButtonOption(type: .block, action: { print("차단하기 clicked") }),
+                                        SheetButtonOption(type: .report, action: { print("신고하기 clicked") }),
+                                        SheetButtonOption(type: .delete, action: { print("삭제하기 clicked") }),
+                                        SheetButtonOption(type: .cancel, action: { print("취소 clicked") })],
+                        sharedString: "모노로그 화이팅",
+                        reportOrDeleteTitle: .memo,
+                        isShowingReportSheet: .constant(false),
+                        isShowingBlockAlert: .constant(false),
+                        isShowingEllipsisSheet: .constant(false),
+                        isShowingDeleteAlert: .constant(false))
+}
