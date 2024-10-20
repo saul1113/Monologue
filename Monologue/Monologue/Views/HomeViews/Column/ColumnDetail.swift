4e4cc96e79cd4d2b36b6414d6d5a03426dd729b0
saul1113 <clclshtus@gmail.com>
2024-10-20 16:21:22 +0900

    [Design]: 칼럼뷰 디테일뷰 시트뷰 재수정

--- Monologue/Monologue/Views/HomeViews/Column/ColumnDetail.swift
+++ Monologue/Monologue/Views/HomeViews/Column/ColumnDetail.swift
@@ -145,21 +145,21 @@ struct ColumnDetail: View {
     }
 }
 
-extension Color {
-    init(_ hex: String) {
-        let scanner = Scanner(string: hex)
-        _ = scanner.scanString("#")
-        
-        var rgb: UInt64 = 0
-        scanner.scanHexInt64(&rgb)
-        
-        let r = Double((rgb >> 16) & 0xFF) / 255.0
-        let g = Double((rgb >> 8) & 0xFF) / 255.0
-        let b = Double(rgb & 0xFF) / 255.0
-        
-        self.init(red: r, green: g, blue: b)
-    }
-}
+//extension Color {
+//    init(_ hex: String) {
+//        let scanner = Scanner(string: hex)
+//        _ = scanner.scanString("#")
+//        
+//        var rgb: UInt64 = 0
+//        scanner.scanHexInt64(&rgb)
+//        
+//        let r = Double((rgb >> 16) & 0xFF) / 255.0
+//        let g = Double((rgb >> 8) & 0xFF) / 255.0
+//        let b = Double(rgb & 0xFF) / 255.0
+//        
+//        self.init(red: r, green: g, blue: b)
+//    }
+//}
 
 //#Preview {
 //    ColumnDetail(column: Column(title: "예시타이틀", content: "Example content", email: "Test Email", userNickname: "북극성", categories: ["에세이"], likes: [], date: Date(), comments: []))
